31a3ef339296cba00fce481684599113960db171
José Welington de Sousa Costa Filho <emailwelingtonsousa@gmail.com>
2017-09-01 11:32:29 -0300

    Commit do exercício 1 balanço trimestral

---
+++ src/exercicio1/BalancoTrimestral.java
@@ -0,0 +1,14 @@
+package exercicio1;
+
+public class BalancoTrimestral {
+
+	public static void main(String... args) {
+		int gastosJaneiro = 15000;
+		int gastosFevereiro = 23000;
+		int gastosMarco = 17000;
+		
+		int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+		
+		System.out.println("Gasto do trimestre: R$" + gastosTrimestre);
+	}
+}
