fc1cee04a280b420701bc3d34a3126a4f53ddd67
José Welington de Sousa Costa Filho <emailwelingtonsousa@gmail.com>
2017-09-02 16:33:02 -0300

    Commit do exercício 4 imprimido soma de 1 até 1000

---
+++ src/exercicio4/ImprimeSoma.java
@@ -0,0 +1,13 @@
+package exercicio4;
+
+public class ImprimeSoma {
+
+    public static void main(String... args) {
+	int soma = 0;
+	for (int elemento = 1; elemento <= 1000; elemento++) {
+	    soma += elemento;
+	}
+	System.out.println("O valor da soma e = " + soma);
+    }
+
+} // 500500
