c4a3ed0a00c0ca80526014029382976edc880f2a
José Welington de Sousa Costa Filho <emailwelingtonsousa@gmail.com>
2017-09-02 18:21:58 -0300

    Commit do exercício 6 e 7 fatorial

--- src/exercicio7/Fatorial.java
+++ src/exercicio7/Fatorial.java
@@ -1,10 +1,10 @@
-package exercicio6;
+package exercicio7;
 
 public class Fatorial {
 
-    public static void main(String... args) {
-	int fatorial = 1;
-	for (int n = 1; n <= 10; n++) {
+    public static void main(String[] args) {
+	long fatorial = 1;
+	for (int n = 1; n <= 30; n++) {
 	    fatorial *= n;
 	    System.out.println("fatorial(" + n + "!" + ") = " + fatorial);
 	}
